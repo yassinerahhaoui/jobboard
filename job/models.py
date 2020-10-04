0bd4388163668602e5617d5fafaa4cee0097ca87
yassinerahhaoui <yassinerahhaoui6@gmail.com>
2020-10-05 00:06:58 +0100

    add modules in project

--- job/models.py
+++ job/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+JOB_TYPE = (
+    ('first_time','first_time'),
+    ('part_time','part_time')
+)
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    #location
+    job_type = models.CharField(max_length=15,choices=JOB_TYPE,default='')
+    description = models.TextField(max_length=1000,default='')
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    #category
+    experience = models.IntegerField(default=1)
+
+
+
+
+    def __str__(self):
+        return self.title
